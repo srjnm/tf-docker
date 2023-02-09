c9b32c07158ff9c71a2948f5d79ec2680ec22537
Suraj <snetlamu@cisco.com>
2023-02-09 11:35:56 +0530

    Edited scripts

--- build-control-image.sh
+++ build-control-image.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 cd docker-files
 docker build -t srjnm/control-ubuntu:v1 -f control-node.Dockerfile .
 docker push srjnm/control-ubuntu:v1
--- build-managed-image.sh
+++ build-managed-image.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 cd docker-files
 docker build -t srjnm/managed-nginx:v2 -f managed-node.Dockerfile .
 docker push srjnm/managed-nginx:v2
